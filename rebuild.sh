1a52ab8f4b6e41716df3ed1d5d7461710bc04cff
skarlsson <svante.karlsson@csi.se>
2018-02-06 11:05:48 +0100

    New chart version

--- rebuild.sh
+++ rebuild.sh
@@ -2,20 +2,20 @@
 
 set -ef
 
-helm lint ../helm-charts/zookeeper
-helm package ../helm-charts/zookeeper
+helm lint ../helm-charts/zookeeper-subchart
+helm package ../helm-charts/zookeeper-subchart
 
-helm lint ../helm-charts/kafka
-helm package ../helm-charts/kafka
+helm lint ../helm-charts/kafka-subchart
+helm package ../helm-charts/kafka-subchart
 
-helm lint ../helm-charts/kafka-op5
-helm package ../helm-charts/kafka-op5
+helm lint ../helm-charts/kafka-op5-subchart
+helm package ../helm-charts/kafka-op5-subchart
 
-helm lint ../helm-charts/kafka-op5-2
-helm package ../helm-charts/kafka-op5-2
+helm lint ../helm-charts/kafka-op5-2-subchart
+helm package ../helm-charts/kafka-op5-2-subchart
 
-helm lint ../helm-charts/schema-registry
-helm package ../helm-charts/schema-registry
+helm lint ../helm-charts/schema-registry-subchart
+helm package ../helm-charts/schema-registry-subchart
 
 helm repo index . 
 git commit --all -m 'New chart version'
@@ -25,13 +25,13 @@ helm dep up ../helm-charts/kafka_cluster
 helm lint ../helm-charts/kafka_cluster
 helm package ../helm-charts/kafka_cluster
 
-helm dep up ../helm-charts/kafka_cluster_op5
-helm lint ../helm-charts/kafka_cluster_op5
-helm package ../helm-charts/kafka_cluster_op5
+#helm dep up ../helm-charts/kafka_cluster_op5
+#helm lint ../helm-charts/kafka_cluster_op5
+#helm package ../helm-charts/kafka_cluster_op5
 
-helm dep up ../helm-charts/kafka_cluster_op5_2
-helm lint ../helm-charts/kafka_cluster_op5_2
-helm package ../helm-charts/kafka_cluster_op5_2
+#helm dep up ../helm-charts/kafka_cluster_op5_2
+#helm lint ../helm-charts/kafka_cluster_op5_2
+#helm package ../helm-charts/kafka_cluster_op5_2
 
 helm repo index . 
 git commit --all -m 'New chart version'
